6c60c56f805e279d0c0e750729ac3a165cba5913
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-05 10:01:08 -0700

    added eslint, deleted semicolons, added get one route

--- routes/dreams.js
+++ routes/dreams.js
@@ -1,12 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var knex = require('../db/connection')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   knex('dreams').then(allDreams => {
-    res.render('index', {allDreams});
+    res.render('index', {allDreams})
   })
-});
+})
 
-module.exports = router;
+router.get('/:id', function (req, res, next) {
+  knex('dreams').where('id', req.params.id).then(thisDream => {
+    res.json(thisDream)
+  })
+  .catch(err => next(err))
+})
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/dreams');
-});
+router.get('/', function (req, res, next) {
+  res.redirect('/dreams')
+})
 
-module.exports = router;
+
+module.exports = router
