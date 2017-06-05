809edb1509bf94708ac3b1cea86030e5541edd07
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-05 10:36:45 -0700

    all routes for dreams working

--- routes/dreams.js
+++ routes/dreams.js
@@ -5,7 +5,7 @@ var knex = require('../db/connection')
 /* GET home page. */
 router.get('/', function (req, res, next) {
   knex('dreams').then(allDreams => {
-    res.render('index', {allDreams})
+    res.json(allDreams)
   })
 })
 
@@ -14,6 +14,27 @@ router.get('/:id', function (req, res, next) {
     res.json(thisDream)
   })
   .catch(err => next(err))
+})
+
+router.post('/', function (req, res, next) {
+  knex('dreams').insert(req.body, '*').then(() => {
+    res.status(201).json(req.body)
+  })
+})
+
+router.put('/:id', function (req, res, next) {
+  knex('dreams').where('id', req.params.id).update(req.body)
+  .then((updatedDream) => {
+    res.status(200).json(req.body)
+  })
+})
+
+router.delete('/:id', function (req, res, next) {
+  console.log(req.params.id)
+  knex('dreams').where('id', req.params.id).del().returning('*')
+  .then((thisDream) => {
+    res.status(210).json(thisDream)
+  })
 })
 
 module.exports = router
