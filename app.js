d641ea1d0ff51eec041d508afb02f73ae6040e58
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-06-04 17:59:43 -0700

    deleted hbs and hbs files, DLed angular and angular-ui

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+const app = express();
+const bodyParser = require('body-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const path = require('path');
 
-var index = require('./routes/index');
-var dreams = require('./routes/dreams');
+const dreams = require('./routes/dreams');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
 
-app.use('/', index)
-app.use('/dreams', dreams)
+app.use('/', dreams)
 
-// catch 404 and forward to error handler
+app.use('*', function(req, res, next) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public', 'angular')})
+})
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -32,11 +30,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  console.log(err);
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/dreams.js
+++ routes/dreams.js
@@ -5,7 +5,7 @@ var knex = require('../db/connection')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   knex('dreams').then(allDreams => {
-    res.render('index', {allDreams});
+    res.json(allDreams);
   })
 });
 
